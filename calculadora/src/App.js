876d287d1daf90bc769e16363d9013f131e24a36
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-11-26 11:17:47 -0300

    Criando os botões principais

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,13 +1,31 @@
 import Input from './Components/Input';
 import Button from './Components/Button';
-import { Container, Content } from './styles'
+import { Container, Content, Row, Column } from './styles'
 
 const App = () => {
   return (
     <Container>
       <Content>
-     <Input />
-     </Content>
+        <Input />
+        <Row>
+                <Button label="7"/>
+                <Button label="8"/>
+                <Button label="9"/>
+                <Button label="-"/>
+            </Row>
+            <Row>
+                <Button label="4"/>
+                <Button label="5"/>
+                <Button label="7"/>
+                <Button label="+"/>
+            </Row>
+            <Row>
+                <Button label="1"/>
+                <Button label="2"/>
+                <Button label="3"/>
+                <Button label="="/>
+            </Row>
+      </Content>
     </Container>
   );
 }
