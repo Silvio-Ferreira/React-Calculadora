6ebc057632b40569126b94455bfaa0d5246a3c79
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-11-27 16:06:54 -0300

    Desenvolvendo Interações nos Botões
    Interações de adicionar o número na tela, e funções para deletar e remover o número inicial "0" ao colocar um novo número

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,29 +1,46 @@
 import Input from './Components/Input';
 import Button from './Components/Button';
 import { Container, Content, Row, Column } from './styles'
+import { useState } from 'react';
+
 
 const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+  };
+  
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${number}${prev === '0' ? '' : prev}`)
+  };
   return (
     <Container>
       <Content>
-        <Input />
+        <Input value={currentNumber} />
         <Row>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="-"/>
+                <Button label="9" onClick={() => handleAddNumber('9')}/>
+                <Button label="÷" onClick={() => handleAddNumber('')}/>
+                <Button label="C" onClick={handleOnClear}/>
+                <Button label="X" onClick={() => handleAddNumber('')}/>
+            </Row>
+        <Row>
+                <Button label="6" onClick={() => handleAddNumber('6')}/>
+                <Button label="7" onClick={() => handleAddNumber('7')}/>
+                <Button label="8" onClick={() => handleAddNumber('8')}/>
+                <Button label="-" onClick={() => handleAddNumber('-')}/>
+          </Row>
+            <Row>
+                <Button label="3" onClick={() => handleAddNumber('3')}/>
+                <Button label="4" onClick={() => handleAddNumber('4')}/>
+                <Button label="5" onClick={() => handleAddNumber('5')}/>
+                <Button label="+" onClick={() => handleAddNumber('')}/>
             </Row>
             <Row>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="7"/>
-                <Button label="+"/>
-            </Row>
-            <Row>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="="/>
+                <Button label="0" onClick={() => handleAddNumber('0')}/>
+                <Button label="1" onClick={() => handleAddNumber('1')}/>
+                <Button label="2" onClick={() => handleAddNumber('2')}/>
+                <Button label="=" onClick={() => handleAddNumber('=')}/>
             </Row>
       </Content>
     </Container>
